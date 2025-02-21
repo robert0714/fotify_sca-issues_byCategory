c00e76dfb12906ad288dd567bf1d152192b77423
Robert <robert0714@gmail.com>
2025-02-21 12:36:53 +0800

    Update WordReportService.java

--- src/main/java/com/example/utils/fotify_sca/issues_byCategory/service/WordReportService.java
+++ src/main/java/com/example/utils/fotify_sca/issues_byCategory/service/WordReportService.java
@@ -17,12 +17,13 @@ public class WordReportService {
 		FortifySCAIssuesByCateGory result =new FortifySCAIssuesByCateGory();		
 		DetailData detailTable = new DetailData();
 		List<RowRenderData> goods = new ArrayList<>();
-		for(int i=0 ; i< data.size() ; ++i) {
+		for (int i = ( data.size()-1); -1< i && i < data.size(); --i) {
 			 IssueByCategory unit = data.get(i);
-			 String[] titles = StringUtils.split(unit.getGroupTitle(),":") ; 
-			 
-			 String riskNameContent = titles[0] + System.lineSeparator() +unit.getProportion();
-			 
+//			 String[] titles = StringUtils.split(unit.getGroupTitle(),":") ; 
+//			 
+//			 String riskNameContent = titles[0] + System.lineSeparator() +unit.getProportion();
+			 String riskNameContent = unit.getGroupTitle() + System.lineSeparator() +unit.getProportion();
+ 	 
 			 RowRenderData good = Rows.of( String.valueOf(1+i), riskNameContent,  unit.getFriority(), "", "/", "" ).center().create();
 			 goods.add(good);
 			 
